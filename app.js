9723cccd302d80cabc01f0c283bf90837f5b7268
Igor Olkhov <smartestbox@gmail.com>
2022-12-03 23:06:14 +0100

    Create the button, that sets size of the box

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const container = document.querySelector('.container');
+const cellsAmountBtn = document.querySelector('.set-cells-amount');
 
-
-function makeGrid (rows, columns) {
+function makeGridBox (sideLength) {
     
-    for(i = 0; i < (rows * columns); i++) {
+    for(i = 0; i < (sideLength ** 2); i++) {
         const div = document.createElement('div');
         div.classList.add('grid-item');
         div.addEventListener('mouseenter', () => {
-            div.style.setProperty('background-color', 'black')
-            div.style.setProperty('border', '1px solid white')
-        })
+            div.style.setProperty('background-color', 'black');
+            div.style.setProperty('border', '1px solid white');
+        });
         container.appendChild(div);
-    }
+    };
 
-    container.style.setProperty('grid-template-rows', `repeat(${rows}, 1fr)`);
-    container.style.setProperty('grid-template-columns', `repeat(${columns}, 1fr)`);
+    container.style.setProperty('grid-template-rows', `repeat(${sideLength}, 1fr)`);
+    container.style.setProperty('grid-template-columns', `repeat(${sideLength}, 1fr)`);
 
-}
+};
 
-makeGrid(prompt('Set the rows', ''), prompt('Set the columns', ''));
-
-
+cellsAmountBtn.addEventListener('click', () => {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild)
+    };
+    
+    makeGridBox(prompt('Set the size of the box', ''));
+});
 
